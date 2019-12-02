9c496e94e1aac7298eaf878eb21619530de68138
Antoine Tréfeu <antoine.rayquaza@gmail.com>
2019-12-02 03:06:54 +0100

    Changed script structure.

--- gpg-messages-manager.sh
+++ gpg-messages-manager.sh
@@ -67,29 +67,6 @@ function checkDirectories() {
 	checkAddr
 }
 
-function showKeys() {
-	echo
-	gpg --list-keys |
-		grep -E '^uid' |
-		sed 's/^.\+\] //' |
-		sed 's/>//' |
-		sed 's/ </;/' |
-		cut -d ';' -f 1- --output-delimiter=$'\t\t\t'
-
-	if [ "$1" == "i" ]; then
-		toMenu
-	fi
-}
-
-function toMenu() {
-	echo; read -p "Press Enter to continue..." foo
-	showMenu
-}
-
-function selectKey() {
-	showKeys | $fzf | cut -f4 -d $'\t'
-}
-
 function checkAddr() {
 	cd $gpgDir
 	if [ -f txt/adr.txt ]; then
@@ -127,107 +104,6 @@ function showMenu() {
 	askOption
 }
 
-function encryptMsg() {
-	cd $gpgDir
-	echo "Please select a recipient : "
-	recipient=$(selectKey)
-
-	if [ "$1" == "adr" ]; then
-		gpg --trust-model always --armor --encrypt --recipient $recipient txt/adr.txt
-
-		if [ $? -eq 0 ]; then
-			recipientFormat=$(echo -n 'adr_for_'; echo $recipient | sed 's/@.\+$//')
-			mv txt/adr.txt.asc asc/$recipientFormat.asc
-			echo -e "\n${green}Address encrypted for ${bs}${recipient}${be}\nto $gpgDir/asc/${bs}$recipientFormat.asc${be} !${end}"
-		else
-			echo -e "\n${red}Warning: An error occured. Please refer to gpg.log.${end}"
-		fi
-
-	else
-		$editor txt/tmp.txt
-		gpg --trust-model always --armor --encrypt --recipient $recipient txt/tmp.txt
-
-		if [ $? -eq 0 ]; then
-			recipientFormat=$(echo -n 'msg_for_'; echo -n $recipient | sed 's/@.\+$/_/'; date +%Y%m%d_%H%M%S)
-			mv txt/tmp.txt.asc asc/$recipientFormat.asc
-			rm txt/tmp.txt
-			echo -e "\n${green}Message encrypted for ${bs}${recipient}${be}\nto $gpgDir/asc/${bs}$recipientFormat.asc${be} !${end}"
-		else
-			echo -e "\n${red}Warning: An error occured. Please refer to gpg.log.${end}"
-		fi
-	fi
-
-	toMenu
-}
-
-function addPubKey() {
-	cd $gpgDir
-	echo "Please select a public key : "
-	public=$(ls pub/ | head | $fzf)
-	gpg --batch --yes --import pub/$public
-
-	if [ $? -eq 0 ]; then
-		echo -e "${green}\nPublic key $public added !${end}"
-		toMenu
-	else
-		echo -e "\n${red}Warning: An error occured. Please refer to gpg.log.${end}"
-	fi
-}
-
-function delPubKey() {
-	echo "Please select a public key : "
-	public=$(selectKey)
-	gpg --batch --yes --delete-key $public
-
-	if [ $? -eq 0 ]; then
-		echo -e "${green}Public key $public deleted !${end}"
-		toMenu
-	else
-		echo -e "\n${red}Warning: An error occured. Please refer to gpg.log.${end}"
-	fi
-}
-
-function genKey() {
-	gpgVersion=$(gpg --version | sed 1q | cut -d ' ' -f3)
-	if [ $(echo -e "2.2.17\n${gpgVersion}" | sort -V | head -n1) == "2.2.17" ]; then
-		# GPG version is greather than 2.2.17
-		gpg --full-generate-key
-	else
-		# GPG version is lower than 2.2.17
-		gpg --gen-key
-	fi
-
-	toMenu
-}
-
-function decryptMsg() {
-	cd $gpgDir
-	messages=$(ls asc/ | grep '^msg' | head)
-	echo "Please select a message : "
-	toDecryptMsg=$(echo -e "$messages" | $fzf)
-	gpg --output txt/$toDecryptMsg.txt --decrypt asc/$toDecryptMsg
-
-	if [ $? -eq 0 ]; then
-		echo RELOADAGENT | gpg-connect-agent
-		echo -e "\n${green}Message successfully decrypted to $gpgDir/txt/${bs}$toDecryptMsg.txt${be} !${end}"
-		echo -e "\nWould you like to see the message now ?\n[Y]es / [N]o\n"
-		read -p ">>> " answerMsg
-
-		while [[ ! $answerMsg == "Y" && ! $answerMsg == "y" && ! $answerMsg == "N" && ! $answerMsg == "n" ]]; do
-			echo -e "\nPlease choose between Y or N !"
-			read -p ">>> " answerMsg
-		done
-
-		if [[ $answerMsg == "Y" || $answerMsg == "y" ]]; then
-			$editor txt/$toDecryptMsg.txt
-		fi
-	else
-		echo -e "\n${red}Warning: An error occured. Please refer to gpg.log.${end}"
-	fi
-
-	toMenu
-}
-
 function askOption() {
 
 	echo; read -p ">>> " choice
@@ -261,6 +137,133 @@ function askOption() {
 		7)
 			decryptMsg;;
 	esac
+}
+
+function showKeys() {
+	if [ "$1" == "i" ]; then
+		echo
+	fi
+
+	gpg --list-keys |
+		grep -E '^uid' |
+		sed 's/^.\+\] //' |
+		sed 's/>//' |
+		sed 's/ </;/' |
+		cut -d ';' -f 1- --output-delimiter=$'\t\t\t'
+
+	if [ "$1" == "i" ]; then
+		toMenu
+	fi
+}
+
+function toMenu() {
+	echo; read -p "Press Enter to continue..." foo
+	showMenu
+}
+
+function selectKey() {
+	showKeys | $fzf | cut -f4 -d $'\t'
+}
+
+function genKey() {
+	gpgVersion=$(gpg --version | sed 1q | cut -d ' ' -f3)
+	if [ $(echo -e "2.2.17\n${gpgVersion}" | sort -V | head -n1) == "2.2.17" ]; then
+		# GPG version is greather than 2.2.17
+		gpg --full-generate-key
+	else
+		# GPG version is lower than 2.2.17
+		gpg --gen-key
+	fi
+
+	toMenu
+}
+
+function addPubKey() {
+	cd $gpgDir
+	echo "Please select a public key : "
+	public=$(ls pub/ | head | $fzf)
+	gpg --batch --yes --import pub/$public
+
+	if [ $? -eq 0 ]; then
+		echo -e "${green}\nPublic key $public added !${end}"
+		toMenu
+	else
+		echo -e "\n${red}Warning: An error occured. Please refer to the gpg error log..${end}"
+	fi
+}
+
+function delPubKey() {
+	echo "Please select a public key : "
+	public=$(selectKey)
+	gpg --batch --yes --delete-key $public
+
+	if [ $? -eq 0 ]; then
+		echo -e "${green}Public key $public deleted !${end}"
+		toMenu
+	else
+		echo -e "\n${red}Warning: An error occured. Please refer to the gpg error log..${end}"
+	fi
+}
+
+function encryptMsg() {
+	cd $gpgDir
+	echo "Please select a recipient : "
+	recipient=$(selectKey)
+
+	if [ "$1" == "adr" ]; then
+		gpg --trust-model always --armor --encrypt --recipient $recipient txt/adr.txt
+
+		if [ $? -eq 0 ]; then
+			recipientFormat=$(echo -n 'adr_for_'; echo $recipient | sed 's/@.\+$//')
+			mv txt/adr.txt.asc asc/$recipientFormat.asc
+			echo -e "\n${green}Address encrypted for ${bs}${recipient}${be}\nto $gpgDir/asc/${bs}$recipientFormat.asc${be} !${end}"
+		else
+			echo -e "\n${red}Warning: An error occured. Please refer to the gpg error log..${end}"
+		fi
+
+	else
+		$editor txt/tmp.txt
+		gpg --trust-model always --armor --encrypt --recipient $recipient txt/tmp.txt
+
+		if [ $? -eq 0 ]; then
+			recipientFormat=$(echo -n 'msg_for_'; echo -n $recipient | sed 's/@.\+$/_/'; date +%Y%m%d_%H%M%S)
+			mv txt/tmp.txt.asc asc/$recipientFormat.asc
+			rm txt/tmp.txt
+			echo -e "\n${green}Message encrypted for ${bs}${recipient}${be}\nto $gpgDir/asc/${bs}$recipientFormat.asc${be} !${end}"
+		else
+			echo -e "\n${red}Warning: An error occured. Please refer to the gpg error log..${end}"
+		fi
+	fi
+
+	toMenu
+}
+
+function decryptMsg() {
+	cd $gpgDir
+	messages=$(ls asc/ | grep '^msg' | head)
+	echo "Please select a message : "
+	toDecryptMsg=$(echo -e "$messages" | $fzf)
+	gpg --output txt/$toDecryptMsg.txt --decrypt asc/$toDecryptMsg
+
+	if [ $? -eq 0 ]; then
+		echo RELOADAGENT | gpg-connect-agent
+		echo -e "\n${green}Message successfully decrypted to $gpgDir/txt/${bs}$toDecryptMsg.txt${be} !${end}"
+		echo -e "\nWould you like to see the message now ?\n[Y]es / [N]o\n"
+		read -p ">>> " answerMsg
+
+		while [[ ! $answerMsg == "Y" && ! $answerMsg == "y" && ! $answerMsg == "N" && ! $answerMsg == "n" ]]; do
+			echo -e "\nPlease choose between Y or N !"
+			read -p ">>> " answerMsg
+		done
+
+		if [[ $answerMsg == "Y" || $answerMsg == "y" ]]; then
+			$editor txt/$toDecryptMsg.txt
+		fi
+	else
+		echo -e "\n${red}Warning: An error occured. Please refer to the gpg error log..${end}"
+	fi
+
+	toMenu
 }
 
 checkDependencies
